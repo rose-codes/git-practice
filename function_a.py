c2c59900f2c0d31a1fb5853a1fd6a7ddfee08f2b
kathawkins <kmoreno1205@gmail.com>
2022-09-23 16:06:42 -0500

    Kat's commit #2

--- function_a.py
+++ function_a.py
@@ -1,6 +1,7 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
+    frequency_index = {}
     max_frequency = -1
     most_common_value = None
     for num in number_list:
